9f7e4b1b4a62b8c529efc8033482da9db57bb747
Eduar Apaza <eapaza@learnsoft.com>
2023-12-27 22:21:25 -0500

    Basic Blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'eduarruben.';
+export const SITE_DESCRIPTION = 'Eduar Ruben Apaza Yanez personal site, programming, reactJS, Astro, Typescript, Javascript, Go, Rust';
